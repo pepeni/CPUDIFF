2f7c9fbc856989ecd0ed33f83c3c76eb4792fd07
pepeni <pepeni1415@gmail.com>
2023-03-13 21:56:16 +0100

    init

---
+++ src/main/java/com/pepeni/springboot/cpudiff/CpudiffApplication.java
@@ -0,0 +1,13 @@
+package com.pepeni.springboot.cpudiff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpudiffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpudiffApplication.class, args);
+	}
+
+}
