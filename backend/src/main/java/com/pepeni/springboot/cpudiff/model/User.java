58b566ca3eb7b4cc4f866198fd52c68bbad14909
pepeni <pepeni1415@gmail.com>
2023-03-21 20:31:01 +0100

    added frontend

---
+++ backend/src/main/java/com/pepeni/springboot/cpudiff/model/User.java
@@ -0,0 +1,39 @@
+package com.pepeni.springboot.cpudiff.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    String email;
+    String nick;
+    String password;
+
+    public User(String email, String nick, String password) {
+        this.email = email;
+        this.nick = nick;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
