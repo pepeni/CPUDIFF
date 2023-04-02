001a7a05a799693a39a6fa0754267cb39aa19e87
pepeni <pepeni1415@gmail.com>
2023-04-02 21:34:31 +0200

    Added CpmmentService with exception

--- src/main/java/com/pepeni/springboot/cpudiff/repository/CommentRepo.java
+++ src/main/java/com/pepeni/springboot/cpudiff/repository/CommentRepo.java
@@ -2,6 +2,14 @@ package com.pepeni.springboot.cpudiff.repository;
 
 import com.pepeni.springboot.cpudiff.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CommentRepo extends JpaRepository<Comment, Long> {
+    Optional<Comment> findCommentById(Long id);
+
+    @Query(value = "SELECT * FROM COMMENT WHERE processor_id = :processorId", nativeQuery = true)
+    Optional<Comment> findCommentByProcessorId(@Param("processorId") Long id);
 }
---
+++ src/main/java/com/pepeni/springboot/cpudiff/service/CommentService.java
@@ -0,0 +1,41 @@
+package com.pepeni.springboot.cpudiff.service;
+
+import com.pepeni.springboot.cpudiff.exception.CommentNotFoundException;
+import com.pepeni.springboot.cpudiff.exception.ProcessorNotFoundException;
+import com.pepeni.springboot.cpudiff.model.Comment;
+import com.pepeni.springboot.cpudiff.model.Processor;
+import com.pepeni.springboot.cpudiff.repository.CommentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+    private final CommentRepo commentRepo;
+
+    @Autowired
+    public CommentService(CommentRepo commentRepo) {
+        this.commentRepo = commentRepo;
+    }
+
+    public Comment addComment(Comment comment){
+        return commentRepo.save(comment);
+    }
+
+    public List<Comment> findAllComments(){
+        return commentRepo.findAll();
+    }
+
+    public Comment findCommentById(Long id){
+        return commentRepo.findCommentById(id).orElseThrow(() -> new CommentNotFoundException("Comment by id = " + id + " was not found"));
+    }
+
+    public Comment findCommentByProcessorId(Long id){
+        return commentRepo.findCommentByProcessorId(id).orElseThrow(() -> new CommentNotFoundException("Comment by Processor id = " + id + " was not found"));
+    }
+
+    public void deleteComment(Long id){
+        commentRepo.deleteById(id);
+    }
+}
