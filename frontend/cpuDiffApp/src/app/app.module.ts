751cf675f65b7629e5feb54ec869eb318b3050dc
pepeni <pepeni1415@gmail.com>
2023-04-03 17:48:04 +0200

    Added new components

--- frontend/cpuDiffApp/src/app/app.module.ts
+++ frontend/cpuDiffApp/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { ComparisonComponent } from './comparison/comparison.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ProcessorComponent } from './processor/processor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    MenuComponent,
+    ComparisonComponent,
+    ArticleComponent,
+    ArticleListComponent,
+    ProcessorComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/cpuDiffApp/src/app/processor.ts
@@ -0,0 +1,8 @@
+export interface Processor{
+    id: number;
+    name: string;
+    cpuClass: string;
+    cores: number;
+    processorClockSpeed: string;
+    processorClockSpeedTurbo: string;
+}
