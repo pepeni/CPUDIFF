942613c69e697f8df8c64e8a59d5ac8a0a157d33
pepeni <pepeni1415@gmail.com>
2023-04-13 11:03:17 +0200

    Added config to article-list.ts and tsconfig.json

--- frontend/cpuDiffApp/src/app/article-list/article-list.component.ts
+++ frontend/cpuDiffApp/src/app/article-list/article-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../article';
+import { ArticleService } from '../article.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.css']
 })
-export class ArticleListComponent {
+export class ArticleListComponent implements OnInit {
+
+  public articles: Article[];
+
+  constructor(private articleService: ArticleService){}
+
+  ngOnInit(): void {
+    this.getArticles();
+  }
+
+  public getArticles(): void {
+    this.articleService.getArticles().subscribe(
+      (response: Article[]) => {
+        this.articles = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 
 }
