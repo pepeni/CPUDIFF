bf9d2f0918d682a66faceba415282d519e969208
pepeni <pepeni1415@gmail.com>
2023-04-19 18:26:26 +0200

    Added processor serching in comparison component

--- frontend/cpuDiffApp/src/app/comparison/comparison.component.ts
+++ frontend/cpuDiffApp/src/app/comparison/comparison.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Processor } from '../processor';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ProcessorService } from '../processor.service';
 
 @Component({
   selector: 'app-comparison',
   templateUrl: './comparison.component.html',
   styleUrls: ['./comparison.component.css']
 })
-export class ComparisonComponent {
+export class ComparisonComponent implements OnInit{
+  public processors: Processor[];
 
+  constructor(private processorService: ProcessorService){}
+
+  ngOnInit(): void {
+    this.getProcessors();
+  }
+
+  public getProcessors(): void {
+    this.processorService.getProcessors().subscribe(
+      (response: Processor[]) => {
+        this.processors = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
